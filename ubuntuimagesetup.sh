934b6906bc58c763b89634cf0b63bd25e09bf68d
BottleD-JinnI <bottledjinni@gmail.com>
2017-09-02 18:26:16 +0300

    Update ubuntuimagesetup.sh

--- ubuntuimagesetup.sh
+++ ubuntuimagesetup.sh
@@ -5,38 +5,28 @@
 # To get the value of qroute-xxxxxxxxxx, RUN 'ip netns list'
 # To get the instance id, run 'openstack server list'. Make sure sourcing the correct file
 
-
+#Assumes 'sudo su'
 #=================set up future logins=================
-#Set up password login. 
 
+#Set up password login. 
 #sudo vim /etc/ssh/sshd_config
 #change 'PasswordAuthentication yes', default = no
 #if automatin it
-sudo su
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 /etc/init.d/ssh restart
 
 #Add new user, and change/set passwords for existing users. 
 useradd -m adminuser
-#sudo passwd ubuntu #password=....
-#sudo passwd adminuser #password=....
-#sudo passwd root #password=....
-#if Automating. 
-#sudo passwd adminuser #
-#sudo passwd ubuntu #
-#sudo passwd root #
 echo -e "password\npassword" | passwd adminuser
 echo -e "password\npassword" | passwd ubuntu
 
 #Make users sudoers
-
 echo ' adminuser ALL=(ALL)   ALL' >> /etc/sudoers
 echo ' ubuntu ALL=(ALL)   ALL' >> /etc/sudoers
 
 #=================Install utilities. 
 apt-get update -y
-apt-get install weechat -y 
-apt-get install vim tree -y
+apt-get install -y weechat vim tree
 apt-get install docker.io -y
 #Or else be root
 usermod -a -G docker adminuser
